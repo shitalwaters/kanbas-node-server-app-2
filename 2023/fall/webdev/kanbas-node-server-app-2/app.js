c64423c27663756c978439ea9d8d2201990d2f35
shetalw <sdwaters8@gmail.com>
2023-12-04 23:57:53 -0500

    A6 finished for noder server

--- 2023/fall/webdev/kanbas-node-server-app-2/app.js
+++ 2023/fall/webdev/kanbas-node-server-app-2/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import session from "express-session";
 import "dotenv/config";
 import Hello from "./hello.js"
 import Lab5 from "./Lab5.js";
@@ -13,10 +14,25 @@ import AssignmentRoutes from './assignments/routes.js';
 const app = express()
 console.log(process.env.FRONTEND_URL);
 app.use(
-    cors()
+    cors({   credentials: true,
+        origin: process.env.FRONTEND_URL})
 );
-
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+  };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+    };
+  }
+  app.use(session(sessionOptions));
+  
 app.use(express.json());
+
 UserRoutes(app);
 AssignmentRoutes(app);
 ModuleRoutes(app);
